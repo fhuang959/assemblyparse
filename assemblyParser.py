2b4d183112bd77b90684f88386a6b73396f82fcc
fhuang959 <fantasticflorathefabulous@gmail.com>
2019-07-08 22:04:18 +0000

    removed call function

--- assemblyParser.py
+++ assemblyParser.py
@@ -9,29 +9,27 @@ def examine(filename):
             line = line.strip()
             #print(line)
             code.append(line)
-    fNames=[]
+    fNames = []
     for line in code:
-        if(line[0]!='.' and line[len(line)-1]==':'):
+        if(line[0] != '.' and line[len(line)-1] == ':'):
             fNames.append(line)
     # print(fNames)
-    dict={}
+    dict = {}
     for function in fNames:
-        dict[function]={'code':[],'fCalled':[]}
+        dict[function] = {'code':[],'fCalled':[]}
     #print(dict)
     bool = False
     for function in fNames:
         for line in code:
-            if(line==function):
+            if(line == function):
                 bool=True
                 continue
             if(bool):
                 dict[function]['code'].append(line)
-                if(line[0:4]=='call'):
+                if(line[0:4] == 'call'):
                     dict[function]['fCalled'].append(line)
-                if(line[0]!='.' and line[len(line)-1]==':'):
-                    bool=False
+                if(line[0] != '.' and line[len(line)-1] == ':'):
+                    bool = False
                     break
     pprint.pprint(dict)
     #print(dict[getExpectedIdentity])   
-
-examine("marginPhase.c.s")
