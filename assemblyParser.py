0def58c6686cbef9507831d8075fc988e6c747a3
Saumil <saumilkt@gmail.com>
2019-07-08 20:47:16 +0000

    added saumil parser

---
+++ assemblyParser.py
@@ -0,0 +1,35 @@
+import pprint
+
+
+def examine(filename):
+    code=[]
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            #print(line)
+            code.append(line)
+    fNames=[]
+    for line in code:
+        if(line[0]!='.' and line[len(line)-1]==':'):
+            fNames.append(line)
+    # print(fNames)
+    dict={}
+    for function in fNames:
+        dict[function]={'code':[],'fCalled':[]}
+    #print(dict)
+    bool = False
+    for function in fNames:
+        for line in code:
+            if(line==function):
+                bool=True
+                continue
+            if(bool):
+                dict[function]['code'].append(line)
+            if(line[0:4]=='call'):
+                dict[function]['fCalled'].append(line)
+            if(line[0]!='.' and line[len(line)-1]==':'):
+                break
+    pprint.pprint(dict)   
+
+examine("marginPhase.c.s")
