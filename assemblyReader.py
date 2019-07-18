4b19000274c6c40137af923b2fad5d3862a376d1
saumilkt <saumilkt@gmail.com>
2019-07-18 12:00:02 -0700

    First attempt at implementation

--- assemblyReader.py
+++ assemblyReader.py
@@ -1,6 +1,15 @@
 import assemblyScalable
+import pprint
 
-def main():
+def getTargetMethod(fNames):
+    s = input("Print target method: "+"\n")
+    if(fNames.count(s)>0):
+        return s
+    else:
+        getTargetMethod(fNames)
+
+
+def dictInit():
   f=[]
   cwd = os.getcwd()
   print(cwd)
@@ -9,5 +18,28 @@ def main():
     if(file.endswith('.c.s') and not file.startswith('m')):
       f.append(file)
   codeDict = examine(f)
+  return codeDict
+
+def dictInit(tDict,codeDict,s):
+    tDict[s]=codeDict[s]
+    return tDict
+
+def recursivelyPrintToDict(codeDict,tFxn,fNames,tDict):
+    for fxn in codeDict[tFxn]['fCalled']:
+        if(fNames.count(fxn)>0):
+            tDict[fxn]=codeDict[fxn]
+            return recursivelyPrintToDict(codeDict,fxn,fNames,tDict)
+
+
+
+
+def main():
+    codeDict=dictInit()
+    fNames=codeDict.getFNames()
+    s = getTargetMethod(fNames)
+    tDict={}
+    tDict=dictInit(tDict,codeDict,s)
+    recursivelyPrintToDict(codeDict,s,fNames,tDict)
+    pprint.pprint(tDict)
 
 main()
